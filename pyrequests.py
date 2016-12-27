7d75f5dc504330fd5f190c093f37a37243617cbd
Ronald Tagolino <tags0516@gmail.com>
2016-12-27 09:58:06 +0000

    Revision. Modify request post.

--- pyrequests.py
+++ pyrequests.py
@@ -7,16 +7,11 @@ from time import time
 
 lock = threading.Lock()
 
-def request_post(s):
-	keywords = []
-	links = []
-	
-	with open("keywords.txt", "r", encoding="utf-8") as f:
-		keywords = [key.rstrip() for key in f]
 
+def request_post(name, s):
 	# upload URL http://imgur.com/upload; need to send request first in captcha URL https://imgur.com/upload/checkcaptcha
 	headers = {
-		'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36', 
+		'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0', 
 		'Referer': 'https://imgur.com/',
 		'Host': 'imgur.com',
 		'Origin': 'https://imgur.com',
@@ -28,11 +23,7 @@ def request_post(s):
 		'total_uploads': 1,
 		'create_album': 'true'
 	}
-	try:
-		upload_captcha_html = s.post(upload_captcha_url, headers=headers, data=data, proxies=proxy, timeout=15)
-	except Exception as e:
-		print("An exception of type {0} occured (Proxy: {1}). Arguments:\n{2!r}".format(type(ex).__name__, proxy, ex.args))
-		upload_captcha_html = s.post(upload_captcha_url, data=data, headers=headers)
+	upload_captcha_html = s.post(upload_captcha_url, data=data, headers=headers)
 	print(upload_captcha_url, upload_captcha_html, upload_captcha_html.content, upload_captcha_html.text, upload_captcha_html.request.headers)
 
 	upload_captcha_response = json.loads(upload_captcha_html.text)
@@ -44,12 +35,7 @@ def request_post(s):
 		'Filedata': ('thumbnail00.png', png_file, 'image/png'),
 		'new_album_id': (None, upload_captcha_response['data']['new_album_id'])	
 	}
-
-	try:
-		post_html = s.post(post_url, files=files, headers=headers, proxies=proxy, timeout=15)
-	except Exception as e:
-		print("An exception of type {0} occured (Proxy: {1}). Arguments:\n{2!r}".format(type(ex).__name__, proxy, ex.args))
-		post_html = s.post(post_url, files=files, headers=headers)
+	post_html = s.post(post_url, files=files, headers=headers)
 	print(post_url, post_html, post_html.text)
 
 	post_html_response = json.loads(post_html.text)
@@ -61,38 +47,33 @@ def request_post(s):
 	# Update image title and description
 	update_url = "http://imgur.com/ajax/titledesc/{}".format(post_html_response['data']['deletehash'])
 	headers = {
-		'User-agent': 'Mozilla/5.0', 
+		'User-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0', 
 		'referer': 'http://imgur.com/{}'.format(post_html_response['data']['album'])
 	}
 	keyword = keywords[random.randint(0, len(keywords) - 1)]
-	
 	data = {
 		'title': keyword,
 		'description': "{}&nbsp;&nbsp;{}\n\n{}".format(keyword, '大奖老虎机 http://www.Q82019309.com', desc_link)
 	}
-	try:
-		update_html = s.post(update_url, data=data, headers=headers, proxies=proxy, timeout=15)
-	except Exception as e:
-		print("An exception of type {0} occured (Proxy: {1}). Arguments:\n{2!r}".format(type(ex).__name__, proxy, ex.args))
-		update_html = s.post(update_url, data=data, headers=headers)
+	update_html = s.post(update_url, data=data, headers=headers)
 	print(update_html)
-	
 
-def request_login(url, data, headers, p):
+
+def request_login(data, headers, p):
+	url = "https://imgur.com/signin?redirect=http%3A%2F%2Fimgur.com%2F"
 	try:
 		s = requests.session()
 		html = s.post(url, data=data, headers=headers, proxies=p, timeout=15)
 		html_tree = etree.HTML(html.content)
 		print(html, html.status_code, data, html_tree.xpath("//div[@class='dropdown-footer']"), html_tree.xpath("//div[@class='captcha']"))
 		if html_tree.xpath("//div[@class='dropdown-footer']"):
-			# accounts_active.append("{}----{}".format(account[0], account[1]))
 			return s
 		else:
 			return None
 		s.cookies.clear()
 		s.close()
 	except Exception as ex:
-		print("{0} An exception of type {1} occured. Arguments:\n{2!r}".format(p, type(ex).__name__, ex.args))
+		print("{0} An exception of type {1} occured. Arguments:\n{2!r}".format(p['ftp'], type(ex).__name__, ex.args))
 		return None
 
 
@@ -165,7 +146,7 @@ def savecontent(links):
 		f.close()
 
 
-def validate_account(name, url, account):
+def validate_account(name, account):
 	for p in ips:
 		if "{}----{}".format(account[0], account[1]) in active_accounts:
 				break
@@ -184,7 +165,7 @@ def validate_account(name, url, account):
 			'ftp': '{}'.format(p)
 		}
 
-		s = request_login(url, data, headers, proxies)
+		s = request_login(data, headers, proxies)
 
 		if type(s).__name__ is "Session":
 			if "{}----{}".format(account[0], account[1]) not in active_accounts:
@@ -196,7 +177,6 @@ def check_accounts():
 
 	active_accounts = []
 	
-	url = "https://imgur.com/signin?redirect=http%3A%2F%2Fimgur.com%2F"
 	threads_num = 2
 
 	for account in accounts:
@@ -204,7 +184,7 @@ def check_accounts():
 		print("Verifying account: {}@{}".format(account[0], account[1]))
 		threads = []
 		for i in range(threads_num):
-			t = threading.Thread(target = validate_account, args = ("Thread-{}".format(i), url, account))
+			t = threading.Thread(target = validate_account, args = ("Thread-{}".format(i), account))
 			threads.append(t)
 			t.start()
 
@@ -231,38 +211,57 @@ def get_user_agents():
 
 
 def main():
+	global active_accounts, keywords, links, start
+
+	active_accounts = []
+	with open("账号active.txt", "r", encoding="utf-8") as f:
+		active_accounts = [tuple(acc.rstrip().split("----")) for acc in f]
 	domain = "http://www.imgur.com"
 	counter = 0
+	keywords = []
+	with open("keywords.txt", "r", encoding="utf-8") as f:
+		keywords = [key.rstrip() for key in f]
+	links = []
 	start = datetime.now()
 	start_time = time()
+	threads_num = 2
 
 	print("Requesting {} ({})".format(domain, start))
 	while True:
-		s = requests.session()
-		account = accounts[counter]
+		account = active_accounts[counter]
 
-		# login
-		login_url = "https://imgur.com/signin?redirect=http%3A%2F%2Fimgur.com%2F"
-		logout_url = ""
-		login_data = {
-			'username': account[0], 
-			'password': account[1]
-		}
-		headers = {
-			'User-agent': 'Mozilla/5.0', 
-			'referer': ''
-		}
-		try:
-			html = s.post(login_url, data=login_data, headers=headers, proxies=proxy, timeout=15)
-		except Exception as e:
-			print("An exception of type {0} occured (Proxy: {1}). Arguments:\n{2!r}".format(type(ex).__name__, proxy, ex.args))
-			html = s.post(login_url, data=login_data, headers=headers)
-		print(login_url, html, login_data)
+		for p in ips:
+			# login
+			print("{}@{} - {}".format(account[0], account[1], p))
+			data = {
+				'username': account[0], 
+				'password': account[1]
+			}
+			headers = {
+				'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+				'referer': ''
+			}
+			proxies = {
+				'http': 'http://{}'.format(p),
+				'https': 'https://{}'.format(p),
+				'ftp': '{}'.format(p)
+			}
+			s = request_login(data, headers, proxies)
 
-		# call post here
+			if type(s).__name__ is "Session":
+				print("{} {} is logged in {}".format(account[0], account[1], p))
 
-		s.cookies.clear()
-		s.close()
+				threads = []
+				for i in range(threads_num):
+					t = threading.Thread(target = request_post, args = ("Thread-{}".format(i), s))
+					threads.append(t)
+					t.start()
+
+				for t in threads:
+					t.join()
+
+				s.cookies.clear()
+				s.close()
 
 		counter += 1
 		if counter >= len(accounts):
@@ -277,7 +276,7 @@ def main():
 if __name__ == "__main__":
 	global accounts, ips, user_agents
 
-	get_proxies()
+	# get_proxies()
 
 	accounts = []
 	with open("账号.txt", "r", encoding='utf-8') as f:
@@ -294,6 +293,5 @@ if __name__ == "__main__":
 		user_agents = [ua.rstrip() for ua in f]
 	print("{} user agents loaded.".format(len(user_agents)))
 
-	check_accounts()
-	# validate() # Validate accounts
-	# main()	
+	# check_accounts()
+	main()
