377ec112c6780de2dabff43d33ad58347967d63f
Ronald Tagolino <tags0516@gmail.com>
2017-01-03 09:17:23 +0000

    Modify flow. Add Rasdial for changing IP.

--- pyrequests.py
+++ pyrequests.py
@@ -22,9 +22,8 @@ def auto_dial():
 
 
 def request_post(name, s, d):
+	auto_dial()
 	try:
-		auto_dial()
-
 		headers = {
 			'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0', 
 			'Referer': 'https://imgur.com/',
@@ -43,10 +42,10 @@ def request_post(name, s, d):
 		upload_captcha_response = json.loads(upload_captcha_html.text)
 
 		post_url = "https://imgur.com/upload"
-		png_file = open("{}/{}".format(image_dir, 'images.jpg'), 'rb')
+		png_file = open("{}/{}".format(image_dir, 'image00.jpg'), 'rb')
 		
 		files = {
-			'Filedata': ('thumbnail00.png', png_file, 'image/jpg'),
+			'Filedata': ('image00.jpg', png_file, 'image/jpg'),
 			'new_album_id': (None, upload_captcha_response['data']['new_album_id'])	
 		}
 		post_html = s.post(post_url, files=files, headers=headers)
@@ -81,15 +80,20 @@ def request_post(name, s, d):
 def request_login(data, headers):
 	auto_dial()
 
+	print("Change IP done.")
 	url = "https://imgur.com/signin?redirect=http%3A%2F%2Fimgur.com%2F"
 	s = requests.session()
 	response = s.post(url, data=data, headers=headers)
-	html = BeautifulSoup(response.content)
+	html = BeautifulSoup(response.content, "html.parser")
+	
 	if html.find("li", {"class": "account"}):
+		print("login for {} is successful.".format(data['username']))
 		return s
-	else:
-		print("Captcha encountered" if html.find("div", {"class", "captcha"}) else "")
+	elif html.find("div", {"class", "captcha"}):
+		print("Captcha encountered for {} login".format(data['username']))
 		return None
+	else:
+		print("Error encountered for {} login {}".format(data['username'], html))
 
 	s.cookies.clear()
 	s.close()
@@ -121,7 +125,7 @@ def validate_accounts(name, account):
 			'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
 			'referer': ''
 		}
-
+		print("Checking if account {} is active.".format(account[0]))
 		s = request_login(data, headers)
 
 		if type(s).__name__ is "Session":
@@ -133,22 +137,26 @@ def validate_accounts(name, account):
 		print(ex)
 
 
-def check_accounts(accs):
+def check_accounts():
 	global active_accounts, inactive_accounts
+
+	accounts = []
+	with open("{}/{}".format(data_dir, "账号.txt"), "r", encoding='utf-8') as f:
+		accounts = [tuple(account.rstrip().split("----")) for account in f]
+	print("{} accounts loaded for checking".format(len(accounts)))
 
 	active_accounts = []
 	inactive_accounts = []
 	
 	threads_num = 2
-	print("Verifying accounts {}".format(accs))
-	temp_accs = accs
+
+	temp_accs = accounts
 	while temp_accs:
 		# login
 		threads = []
 		for i in range(threads_num):
 			if temp_accs:
 				acc = temp_accs.pop()
-				print(acc)
 				if "{}----{}".format(acc[0], acc[1]) not in active_accounts:
 					t = threading.Thread(target = validate_accounts, args = ("Thread-{}".format(i), acc))
 					threads.append(t)
@@ -179,6 +187,7 @@ def main():
 	active_accounts = []
 	with open("{}/{}".format(data_dir, "账号active.txt"), "r", encoding="utf-8") as f:
 		active_accounts = [tuple(acc.rstrip().split("----")) for acc in f]
+	print("{} accounts loaded to process upload.".format(len(active_accounts)))
 	domain = "http://www.imgur.com"
 	counter = 0
 	keywords = []
@@ -194,57 +203,54 @@ def main():
 	print("Requesting {} ({})".format(domain, start))
 	try:
 		while True:
-			account = active_accounts[counter]
+			if active_accounts:
+				account = active_accounts[counter]
 
-			# login
-			data = {
-				'username': account[0], 
-				'password': account[1]
-			}
-			headers = {
-				'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
-				'referer': ''
-			}
-
-			if account[0] not in rep['upload_summary']:
-				rep['upload_summary'][account[0]] = {
-					'links': [],
-					'total_post': 0
+				# login
+				data = {
+					'username': account[0], 
+					'password': account[1]
+				}
+				headers = {
+					'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+					'referer': ''
 				}
 
-			s = request_login(data, headers, proxies)
-
-			if type(s).__name__ is "Session":
-				threads = []
-				for i in range(threads_num):
-					d = {
-						'username': account[0]
+				if account[0] not in rep['upload_summary']:
+					rep['upload_summary'][account[0]] = {
+						'links': [],
+						'total_post': 0
 					}
 
-					t = threading.Thread(target = request_post, args = ("Thread-{}".format(i), s, d))
-					threads.append(t)
-					t.start()
+				print("logging in: {}".format(account[0]))
+				s = request_login(data, headers)
 
-				for t in threads:
-					t.join()
+				if type(s).__name__ is "Session":
+					threads = []
+					for i in range(threads_num):
+						d = {
+							'username': account[0]
+						}
 
-				lock.acquire()
-				lock.release()
+						t = threading.Thread(target = request_post, args = ("Thread-{}".format(i), s, d))
+						threads.append(t)
+						t.start()
 
-				s.cookies.clear()
-				s.close()
+					for t in threads:
+						t.join()
+
+					lock.acquire()
+					lock.release()
+
+					s.cookies.clear()
+					s.close()
 
 			counter += 1
 			if counter >= len(active_accounts):
 				if len(links) == 0:
-					print("You are uploading os fast.")
+					print("You are uploading so fast.")
 					break
 				counter = 0
-
-			if (time() - start_time) >= 7200:
-				savecontent(links)
-				print("Upload is running for 30m. Stop!")
-				break
 	except KeyboardInterrupt:
 		savecontent(links)
 		print(rep)
@@ -253,19 +259,13 @@ def main():
 		print(rep)
 		print("error {} ({})".format(type(ex).__name__, datetime.now() - start))
 
-
 if __name__ == "__main__":
-	global accounts, data_dir, image_dir
+	global data_dir, image_dir
 	data_dir = "data"
 	image_dir = "images"
 
-	accounts = []
-	with open("{}/{}".format(data_dir, "账号.txt"), "r", encoding='utf-8') as f:
-		accounts = [tuple(account.rstrip().split("----")) for account in f]
-	print("{} accounts loaded".format(len(accounts)))
-
 	while True:
-		check_accounts(accounts)
+		check_accounts()
 		main()
 
 		print("Sleeping for 1hr...")
